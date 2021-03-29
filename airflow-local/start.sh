6b0143bb1b420ad1628b569d9aff780cd500a3f7
Dmitry Akatov <akatovda@yandex.com>
2021-03-29 16:49:15 +0300

    start script

--- airflow-local/start.sh
+++ airflow-local/start.sh
@@ -1,4 +1,15 @@
 #!/usr/bin/env bash
 
+export AIRFLOW_HOME=$PWD
+
 airflow db init
+
+airflow users create \
+        --username admin \
+        --password admin \
+        --firstname Geek \
+        --lastname Brain \
+        --role Admin \
+        --email email@address.com
+
 airflow scheduler & airflow webserver -p 8080 && fg
