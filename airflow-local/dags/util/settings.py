79e677e38816867db2a7c7b6093e3850a8686c7d
Dmitry Akatov <akatovda@yandex.com>
2021-04-01 18:21:29 +0300

    upgrade

--- airflow-local/dags/util/settings.py
+++ airflow-local/dags/util/settings.py
@@ -9,11 +9,11 @@ def default_settings():
     filename = module.__file__
 
     settings = {
-        'dag_id': pathlib.Path(filename).stem,  # Имя DAG
+        'dag_id': pathlib.Path(filename).stem,  # Имя DAG достаем из имени файла
         'schedule_interval': '@daily',  # Периодичность запуска, например, "00 15 * * *"
-        'catchup': False,
+        'catchup': False,  # Выполняем только последний запуск
         'default_args': {  # Базовые аргументы для каждого оператора
-            'owner': 'airflow',  # Информация о владельце DAG
+            'owner': 'geekbrains',  # Информация о владельце DAG
             'start_date': dt.datetime(2020, 12, 23),  # Время начала выполнения пайплайна
             'retries': 1,  # Количество повторений в случае неудач
             'retry_delay': dt.timedelta(minutes=1),  # Пауза между повторами
