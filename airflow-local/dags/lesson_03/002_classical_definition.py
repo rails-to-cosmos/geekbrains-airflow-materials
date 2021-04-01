4292db7f408338a10d7455cd9968003eb0603639
Dmitry Akatov <akatovda@yandex.com>
2021-04-01 19:15:13 +0300

    defs

--- airflow-local/dags/lesson_03/002_classical_definition.py
+++ airflow-local/dags/lesson_03/002_classical_definition.py
@@ -4,13 +4,16 @@ from airflow.operators.python_operator import PythonOperator
 from util.settings import default_settings
 
 
-def python_operator_task(**context):
-    print('Hey world, I\'m a classic python operator!')
+def python_operator_task(value: str, **context):
+    print('Hey world, I\'m a classic python operator! Value = ', value)
 
 
 with DAG(**default_settings()) as dag:
     classic_python_operator_definition = PythonOperator(
         task_id='classic_python_operator',
         python_callable=python_operator_task,
+        op_kwargs={
+            'value': 'python operator value',
+        },
         dag=dag,
     )
--- airflow-local/dags/lesson_03/003_decorated_definition.py
+++ airflow-local/dags/lesson_03/003_decorated_definition.py
@@ -5,8 +5,8 @@ from util.deco import python_operator
 
 
 @python_operator()
-def decorated_python_operator_definition(**context):
-    print('Hey I\'m a decorated operator')
+def decorated_python_operator_definition(value, **context):
+    print('Hey I\'m a decorated operator. Value = ', value)
 
 
 with DAG(**default_settings()) as dag:
